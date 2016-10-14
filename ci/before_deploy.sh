03c5096f31d350bf02bfe078da8bd1b3558ddc30
Matt Green <mattgreenrocks@gmail.com>
2016-10-13 22:09:34 -0400

    Add CI scripts

---
+++ ci/before_deploy.sh
@@ -0,0 +1,14 @@
+# Build script shamelessly stolen from ripgrep :)
+
+cargo build --target $TARGET --release
+
+build_dir=$(mktemp -d 2>/dev/null || mktemp -d -t tmp)
+out_dir=$(pwd)
+name="${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}"
+mkdir "$build_dir/$name"
+
+cp target/$TARGET/release/watchexec "$build_dir/$name/"
+
+pushd $build_dir
+tar czf "$out_dir/$name.tar.gz" *
+popd
---
+++ ci/script.sh
@@ -0,0 +1,3 @@
+cargo clean --target $TARGET --verbose
+cargo build --target $TARGET --verbose
+cargo test --target $TARGET --verbose
