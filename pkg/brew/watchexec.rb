09386a61b172db9c2b418458ebcf9bac2da5e1ca
Matt Green <mattgreenrocks@gmail.com>
2016-10-15 11:15:11 -0400

    Update Homebrew formula

--- pkg/brew/watchexec.rb
+++ pkg/brew/watchexec.rb
@@ -1,11 +1,9 @@
 class Watchexec < Formula
   desc "Execute commands when watched files change"
   homepage "https://github.com/mattgreen/watchexec"
-  url "https://github.com/mattgreen/watchexec/releases/download/0.11.0/watchexec_osx_0.11.0.tar.gz"
-  version "0.11.0"
-  sha256 "eb11d74ccaff973768e31a7f290c42b77c1f1faeb3c93970cf4f93285af64c39"
-
-  bottle :unneeded
+  url "https://github.com/mattgreen/watchexec/releases/download/1.0.0/watchexec-1.0.0-x86_64-apple-darwin.tar.gz"
+  version "1.0.0"
+  sha256 "151d8f8075dfb88a69b1d5d6f9b43c849aa1e099a3357fab494b1f43092ed59c"
 
   def install
     bin.install "watchexec"
