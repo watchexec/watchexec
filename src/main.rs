6887552aab21e1567848dfec9e5d892827d17688
Tom Parker <palfrey@tevp.net>
2016-10-18 22:11:05 +0100

    Option to run command initially, before first file change (fixes #12)

--- src/main.rs
+++ src/main.rs
@@ -71,6 +71,9 @@ fn get_args<'a>() -> ArgMatches<'a> {
         .arg(Arg::with_name("no-vcs-ignore")
              .help("Skip auto-loading of .gitignore files for filtering")
              .long("no-vcs-ignore"))
+        .arg(Arg::with_name("run-initially")
+             .help("Run command initially, before first file change")
+             .long("run-initially"))
         .get_matches()
 }
 
@@ -153,6 +156,9 @@ fn main() {
     let mut runner = Runner::new(args.is_present("restart"), args.is_present("clear"));
 
     loop {
+        if args.is_present("run-initially") {
+            runner.run_command(&cmd);
+        }
         let e = wait(&rx, &filter).expect("error when waiting for filesystem changes");
 
         debug!("{:?}: {:?}", e.op, e.path);
