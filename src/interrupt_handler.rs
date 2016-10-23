7b766b1f98f375ec367df1f5c511817650259b2f
Matt Green <mattgreenrocks@gmail.com>
2016-10-23 15:15:28 -0400

    Drop signal crate in favor of nix

--- src/interrupt_handler.rs
+++ src/interrupt_handler.rs
@@ -1,23 +1,29 @@
 use std::sync::Mutex;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::mpsc::{channel, Receiver, Sender, SendError};
 
 lazy_static! {
     static ref INTERRUPT_TX: Mutex<Option<Sender<()>>> = Mutex::new(None);
+    static ref INTERRUPT_REQUESTED: AtomicBool = AtomicBool::new(false);
 }
 
-/// On Unix platforms, spawn a thread and use the signal crate
-/// to relay signals back to the main thread.
-#[cfg(unix)]
+/// On Unix platforms, mask reception of SIGINT/SIGTERM, spawn a thread,
+/// and sigwait on those signals to safely relay them.
 pub fn install() -> Receiver<()> {
     use std::thread;
-    use nix::sys::signal::{SIGTERM, SIGINT};
-    use signal::trap::Trap;
+    use nix::sys::signal::{SigSet, SIGTERM, SIGINT};
 
-    let trap = Trap::trap(&[SIGTERM, SIGINT]);
+    let mut mask = SigSet::empty();
+    mask.add(SIGTERM).expect("unable to add SIGTERM to mask");
+    mask.add(SIGINT).expect("unable to add SIGINT to mask");
+    mask.thread_set_mask().expect("unable to set signal mask");
+
     let rx = create_channel();
 
     thread::spawn(move || {
-        for _ in trap {
+        loop {
+            let _ = mask.wait().expect("unable to sigwait");
+
             let result = send_interrupt();
             if result.is_err() {
                 break;
@@ -48,6 +54,10 @@ pub fn install() -> Receiver<()> {
     rx
 }
 
+pub fn interrupt_requested() -> bool {
+    INTERRUPT_REQUESTED.load(Ordering::Relaxed)
+}
+
 fn create_channel() -> Receiver<()> {
     let mut guard = INTERRUPT_TX.lock().unwrap();
     if (*guard).is_some() {
@@ -61,6 +71,8 @@ fn create_channel() -> Receiver<()> {
 }
 
 fn send_interrupt() -> Result<(), SendError<()>> {
+    INTERRUPT_REQUESTED.store(true, Ordering::Relaxed);
+
     if let Some(ref mut tx) = *INTERRUPT_TX.lock().unwrap() {
         tx.send(())
     } else {
