3ed6906aa473cce5cdf40dae5fdd82da67f96d57
Matt Green <mattgreenrocks@gmail.com>
2016-10-29 13:16:31 -0400

    Package man page and LICENSE with executable

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -8,6 +8,7 @@ name="${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}"
 mkdir "$build_dir/$name"
 
 cp target/$TARGET/release/watchexec "$build_dir/$name/"
+cp {doc/watchexec.1,LICENSE} "$build_dir/$name/"
 
 pushd $build_dir
 tar czf "$out_dir/$name.tar.gz" *
