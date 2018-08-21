ef0e6df8d4a40dd85280e891d98351a833aee3a6
Félix Saparelli <felix@passcod.name>
2018-08-22 00:10:57 +1200

    Remove duplication between main/lib files

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,6 @@
-#[macro_use]
-extern crate clap;
-extern crate globset;
-extern crate env_logger;
-#[macro_use]
-extern crate log;
-#[macro_use]
-extern crate lazy_static;
-extern crate notify;
-
-#[cfg(unix)]
-extern crate nix;
-#[cfg(windows)]
-extern crate winapi;
-#[cfg(windows)]
-extern crate kernel32;
-
-#[cfg(test)]
-extern crate mktemp;
-
-mod cli;
-mod gitignore;
-mod notification_filter;
-mod process;
-mod run;
-mod signal;
-mod watcher;
-mod pathop;
+extern crate watchexec;
+use watchexec::{cli::get_args, run};
 
 fn main() {
-    let args = cli::get_args();
-    run::run(args);
+    run(get_args());
 }
