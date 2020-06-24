ebd5ec084f99b3a7a3db2877e181a5e936f6c805
Philip Daniels <Philip.Daniels1971@gmail.com>
2020-06-24 15:17:59 +0100

    Do not call init_logger from inside watch.
    And move init_logger to main.rs for future use.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+use std::io::Write;
+
 extern crate watchexec;
 
 use watchexec::{cli, error, run};
 
 fn main() -> error::Result<()> {
     run(cli::get_args()?)
+}
+
+fn init_logger(debug: bool) {
+    let mut log_builder = env_logger::Builder::new();
+    let level = if debug {
+        log::LevelFilter::Debug
+    } else {
+        log::LevelFilter::Warn
+    };
+
+    log_builder
+        .format(|buf, r| writeln!(buf, "*** {}", r.args()))
+        .filter(None, level)
+        .init();
 }
