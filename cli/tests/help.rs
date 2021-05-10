0d2f28c91a551280ea5e04e64c5823059a8f8c23
Félix Saparelli <felix@passcod.name>
2021-05-10 23:02:31 +1200

    Add test for cli help

---
+++ cli/tests/help.rs
@@ -0,0 +1,17 @@
+use std::process::Command;
+
+use assert_cmd::prelude::*;
+use insta::assert_snapshot;
+
+#[test]
+fn help() {
+    let output = Command::cargo_bin("watchexec")
+        .unwrap()
+        .arg("--help")
+        .output()
+        .unwrap();
+
+    assert!(output.status.success(), "--help returns 0");
+    assert_eq!(output.stderr, Vec::<u8>::new(), "--help stderr is empty");
+    assert_snapshot!(if cfg!(windows) { "help_windows" } else { "help_unix" }, String::from_utf8(output.stdout).unwrap());
+}
