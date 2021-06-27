08d795f84179d48a49d7b027d54f4f19563b4584
Félix Saparelli <felix@passcod.name>
2021-06-27 15:13:15 +1200

    Remove stub main.rs from library

--- lib/src/main.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(not(debug_assertions))]
-compile_error!("The watchexec CLI has moved to the watchexec-cli crate");
-fn main() {
-    panic!("The watchexec CLI has moved to the watchexec-cli crate");
-}
