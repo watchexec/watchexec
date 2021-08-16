f08bdad8ee8946622479d2a3f19db5866c332f62
Félix Saparelli <felix@passcod.name>
2021-08-16 21:52:13 +1200

    Use local datetime on events instead of naive

--- lib/src/event.rs
+++ lib/src/event.rs
@@ -4,7 +4,7 @@
 //! 2. To be filtered upon according to whatever set of criteria is desired;
 //! 3. To carry information about what caused the event, which may be provided to the process.
 
-use chrono::NaiveDateTime;
+use chrono::{DateTime, Local};
 use std::{collections::HashMap, path::PathBuf};
 
 /// An event, as far as watchexec cares about.
@@ -18,7 +18,7 @@ pub struct Event {
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[non_exhaustive]
 pub enum Particle {
-	Time(NaiveDateTime),
+	Time(DateTime<Local>),
 	Path(PathBuf),
 	Source(Source),
 	Process(u32),
