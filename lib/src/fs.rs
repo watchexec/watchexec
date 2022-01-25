4bf75cdf027729894dd4712aabf4abf976e1a444
Félix Saparelli <felix@passcod.name>
2022-01-26 01:48:21 +1300

    Add optional filterer to fs working data

--- lib/src/fs.rs
+++ lib/src/fs.rs
@@ -15,7 +15,7 @@ use tracing::{debug, error, trace, warn};
 
 use crate::{
 	error::{CriticalError, RuntimeError},
-	event::{Event, Source, Tag},
+	event::{Event, Source, Tag}, filter::Filterer,
 };
 
 /// What kind of filesystem watcher to use.
@@ -76,6 +76,20 @@ pub struct WorkingData {
 
 	/// The kind of watcher to be used.
 	pub watcher: Watcher,
+
+	/// The filterer implementation to use when filtering paths to watch.
+	///
+	/// This is invoked in this context in a special way, as synthetic events
+	/// with [`Source::Internal`] and a single path. This is used to filter out
+	/// paths that are not to be watched, in the context of a recursive
+	/// filesystem watch where we control the descent.
+	///
+	/// Even though the default [`Filterer`] is a noop, there is no way to check
+	/// that the filterer is always a noop, so this is an [`Option`]: if `Some`,
+	/// the internal watcher implementation will use non-recursive watching and
+	/// this module does the descent itself, otherwise the recursion is left to
+	/// the discretion of the Notify library.
+	pub filterer: Option<Arc<dyn Filterer>>,
 }
 
 /// A path to watch.
