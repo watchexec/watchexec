6506664d2156f3d650784daec724ef19a67aa0f7
Félix Saparelli <felix@passcod.name>
2022-07-11 23:33:56 +1200

    Move mimalloc allocator init to CLI

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,7 @@
+#[cfg(target_env = "musl")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 #[tokio::main]
 async fn main() -> miette::Result<()> {
 	watchexec_cli::run().await
