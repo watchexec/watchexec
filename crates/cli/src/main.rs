5850c284ca484ab6dbe354fdc9eb19fc8f69c870
Félix Saparelli <felix@passcod.name>
2023-02-15 11:29:54 +1300

    Exit explicitly when we're PID 1 (#497)

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -4,5 +4,11 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[tokio::main]
 async fn main() -> miette::Result<()> {
-	watchexec_cli::run().await
+	watchexec_cli::run().await?;
+
+	if std::process::id() == 1 {
+		std::process::exit(0);
+	}
+
+	Ok(())
 }
