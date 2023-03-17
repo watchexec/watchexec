da56bf490a8c0b3cfe8c3d2e082afab611e0a0c7
Félix Saparelli <felix@passcod.name>
2023-03-17 22:46:43 +1300

    Test parse and generate at the same time

--- crates/events/tests/json.rs
+++ crates/events/tests/json.rs
@@ -1,14 +1,75 @@
 use snapbox::assert_eq_path;
-use watchexec_events::{Event, Source, Tag};
+use watchexec_events::{Event, Keyboard, ProcessEnd, Source, Tag};
+
+fn parse_file(path: &str) -> Vec<Event> {
+	serde_json::from_str(&std::fs::read_to_string(path).unwrap()).unwrap()
+}
 
 #[test]
-fn generate_single() {
-	let event = Event {
+fn single() {
+	let single = Event {
 		tags: vec![Tag::Source(Source::Internal)],
 		metadata: Default::default(),
 	};
+
 	assert_eq_path(
-		"tests/snapshots/generate_single.json",
-		serde_json::to_string_pretty(&event).unwrap(),
+		"tests/snapshots/single.json",
+		serde_json::to_string_pretty(&single).unwrap(),
 	);
+
+	assert_eq!(
+		serde_json::from_str::<Event>(
+			&std::fs::read_to_string("tests/snapshots/single.json").unwrap()
+		)
+		.unwrap(),
+		single
+	);
+}
+
+#[test]
+fn array() {
+	let array = &[
+		Event {
+			tags: vec![Tag::Source(Source::Internal)],
+			metadata: Default::default(),
+		},
+		Event {
+			tags: vec![Tag::ProcessCompletion(Some(ProcessEnd::Success))],
+			metadata: Default::default(),
+		},
+		Event {
+			tags: vec![Tag::Keyboard(Keyboard::Eof)],
+			metadata: Default::default(),
+		},
+	];
+
+	assert_eq_path(
+		"tests/snapshots/array.json",
+		serde_json::to_string_pretty(array).unwrap(),
+	);
+
+	assert_eq!(parse_file("tests/snapshots/array.json"), array);
+}
+
+#[test]
+fn sources() {
+	let sources = &[
+		Source::Filesystem,
+		Source::Keyboard,
+		Source::Mouse,
+		Source::Os,
+		Source::Time,
+		Source::Internal,
+	]
+	.map(|source| Event {
+		tags: vec![Tag::Source(source)],
+		metadata: Default::default(),
+	});
+
+	assert_eq_path(
+		"tests/snapshots/sources.json",
+		serde_json::to_string_pretty(sources).unwrap(),
+	);
+
+	assert_eq!(parse_file("tests/snapshots/sources.json"), sources);
 }
