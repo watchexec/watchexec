9e6479738bf3fafa3fc69285f496a4b25fa4d871
Félix Saparelli <felix@passcod.name>
2023-11-26 01:57:49 +1300

    Fix supervisor tests

--- crates/supervisor/tests/programs.rs
+++ crates/supervisor/tests/programs.rs
@@ -1,12 +1,15 @@
 use command_group::AsyncCommandGroup;
-use watchexec_supervisor::command::{Program, Shell};
+use watchexec_supervisor::command::{Command, Program, Shell};
 
 #[tokio::test]
 #[cfg(unix)]
 async fn unix_shell_none() -> Result<(), std::io::Error> {
-	assert!(Program::Exec {
-		prog: "echo".into(),
-		args: vec!["hi".into()],
+	assert!(Command {
+		program: Program::Exec {
+			prog: "echo".into(),
+			args: vec!["hi".into()],
+		},
+		options: Default::default()
 	}
 	.to_spawnable()
 	.group_status()
@@ -18,10 +21,13 @@ async fn unix_shell_none() -> Result<(), std::io::Error> {
 #[tokio::test]
 #[cfg(unix)]
 async fn unix_shell_sh() -> Result<(), std::io::Error> {
-	assert!(Program::Shell {
-		shell: Shell::new("sh"),
-		command: "echo hi".into(),
-		args: Vec::new(),
+	assert!(Command {
+		program: Program::Shell {
+			shell: Shell::new("sh"),
+			command: "echo hi".into(),
+			args: Vec::new(),
+		},
+		options: Default::default()
 	}
 	.to_spawnable()
 	.group_status()
@@ -33,10 +39,13 @@ async fn unix_shell_sh() -> Result<(), std::io::Error> {
 #[tokio::test]
 #[cfg(unix)]
 async fn unix_shell_alternate() -> Result<(), std::io::Error> {
-	assert!(Program::Shell {
-		shell: Shell::new("bash"),
-		command: "echo".into(),
-		args: vec!["--".into(), "hi".into()],
+	assert!(Command {
+		program: Program::Shell {
+			shell: Shell::new("bash"),
+			command: "echo".into(),
+			args: vec!["--".into(), "hi".into()],
+		},
+		options: Default::default()
 	}
 	.to_spawnable()
 	.group_status()
@@ -48,13 +57,16 @@ async fn unix_shell_alternate() -> Result<(), std::io::Error> {
 #[tokio::test]
 #[cfg(unix)]
 async fn unix_shell_alternate_shopts() -> Result<(), std::io::Error> {
-	assert!(Program::Shell {
-		shell: Shell {
-			options: vec!["-o".into(), "errexit".into()],
-			..Shell::new("bash")
+	assert!(Command {
+		program: Program::Shell {
+			shell: Shell {
+				options: vec!["-o".into(), "errexit".into()],
+				..Shell::new("bash")
+			},
+			command: "echo hi".into(),
+			args: Vec::new(),
 		},
-		command: "echo hi".into(),
-		args: Vec::new(),
+		options: Default::default()
 	}
 	.to_spawnable()
 	.group_status()
@@ -66,9 +78,12 @@ async fn unix_shell_alternate_shopts() -> Result<(), std::io::Error> {
 #[tokio::test]
 #[cfg(windows)]
 async fn windows_shell_none() -> Result<(), std::io::Error> {
-	assert!(Program::Exec {
-		prog: "echo".into(),
-		args: vec!["hi".into()],
+	assert!(Command {
+		program: Program::Exec {
+			prog: "echo".into(),
+			args: vec!["hi".into()],
+		},
+		options: Default::default()
 	}
 	.to_spawnable()
 	.unwrap()
@@ -81,10 +96,13 @@ async fn windows_shell_none() -> Result<(), std::io::Error> {
 #[tokio::test]
 #[cfg(windows)]
 async fn windows_shell_cmd() -> Result<(), std::io::Error> {
-	assert!(Program::Shell {
-		shell: Shell::cmd(),
-		args: Vec::new(),
-		command: r#""echo" hi"#.into()
+	assert!(Command {
+		program: Program::Shell {
+			shell: Shell::cmd(),
+			args: Vec::new(),
+			command: r#""echo" hi"#.into()
+		},
+		options: Default::default()
 	}
 	.to_spawnable()
 	.unwrap()
@@ -97,10 +115,13 @@ async fn windows_shell_cmd() -> Result<(), std::io::Error> {
 #[tokio::test]
 #[cfg(windows)]
 async fn windows_shell_powershell() -> Result<(), std::io::Error> {
-	assert!(Program::Shell {
-		shell: Shell::new("pwsh.exe"),
-		args: Vec::new(),
-		command: "echo hi".into()
+	assert!(Command {
+		program: Program::Shell {
+			shell: Shell::new("pwsh.exe"),
+			args: Vec::new(),
+			command: "echo hi".into()
+		},
+		options: Default::default()
 	}
 	.to_spawnable()
 	.unwrap()
