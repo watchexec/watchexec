d713b2e4a3f317672658dbd3f2057e4b9df74d84
Félix Saparelli <felix@passcod.name>
2023-11-29 21:32:03 +1300

    cli integration tests

---
+++ crates/cli/run-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export WATCHEXEC_BIN=$(realpath ${WATCHEXEC_BIN:-$(which watchexec)})
+
+cd "$(dirname "${BASH_SOURCE[0]}")/integration"
+for test in *.sh; do
+	echo "Testing $test"
+	./$test
+done
