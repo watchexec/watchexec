ff5e33d9e431e6abc205374ccc3daa54fa7d8a3e
Félix Saparelli <felix@passcod.name>
2024-04-23 11:23:18 +1200

    doc: fix --on-busy-update help text

--- crates/cli/src/args.rs
+++ crates/cli/src/args.rs
@@ -145,17 +145,14 @@ pub struct Args {
 
 	/// What to do when receiving events while the command is running
 	///
-	/// Default is to 'queue' up events and run the command once again when the previous run has
-	/// finished. You can also use 'do-nothing', which ignores events while the command is running
-	/// and may be useful to avoid spurious changes made by that command, or 'restart', which
-	/// terminates the running command and starts a new one. Finally, there's 'signal', which only
-	/// sends a signal; this can be useful with programs that can reload their configuration without
-	/// a full restart.
+	/// Default is to 'do-nothing', which ignores events while the command is running, so that
+	/// changes that occur due to the command are ignored, like compilation outputs. You can also
+	/// use 'queue' which will run the command once again when the current run has finished if any
+	/// events occur while it's running, or 'restart', which terminates the running command and starts
+	/// a new one. Finally, there's 'signal', which only sends a signal; this can be useful with
+	/// programs that can reload their configuration without a full restart.
 	///
 	/// The signal can be specified with the '--signal' option.
-	///
-	/// Note that this option is scheduled to change its default to 'do-nothing' in the next major
-	/// release. File an issue if you have any concerns.
 	#[arg(
 		short,
 		long,
