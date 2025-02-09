d73393598122b94fe2373ffea1661ffc3a403c65
Félix Saparelli <felix@passcod.name>
2025-02-09 19:20:39 +1300

    actually test the thing

--- crates/cli/integration/socket.sh
+++ crates/cli/integration/socket.sh
@@ -4,4 +4,7 @@ set -euxo pipefail
 
 watchexec=${WATCHEXEC_BIN:-watchexec}
 
-timeout -s9 30s sh -c "sleep 10 | $watchexec --socket 18080 --socket 28080 echo"
+timeout -s9 30s sh -c "sleep 10 | $watchexec --socket 18080 --socket 28080 -1 'env | tee fd-env'"
+
+grep LISTEN_FDS=2 fd-env
+
