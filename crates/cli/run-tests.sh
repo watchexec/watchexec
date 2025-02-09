ad086da7ce6174c660a1a5c6b06117dbc5a3379b
Félix Saparelli <felix@passcod.name>
2025-02-09 21:24:15 +1300

    run integration tests on windows

--- crates/cli/run-tests.sh
+++ crates/cli/run-tests.sh
@@ -3,9 +3,21 @@
 set -euo pipefail
 
 export WATCHEXEC_BIN=$(realpath ${WATCHEXEC_BIN:-$(which watchexec)})
+export TEST_SOCKETFD_BIN=$(realpath ${TEST_SOCKETFD_BIN:-$(which test-socketfd)})
+
+platform="${1:-unix}"
 
 cd "$(dirname "${BASH_SOURCE[0]}")/integration"
 for test in *.sh; do
+	if [[ "$test" == *-unix.sh && "$platform" = "windows" ]]; then
+		echo "Skipping $test as it requires unix"
+		continue
+	fi
+	if [[ "$test" == *-win.sh && "$platform" != "windows" ]]; then
+		echo "Skipping $test as it requires windows"
+		continue
+	fi
+
 	echo
 	echo
 	echo "======= Testing $test ======="
