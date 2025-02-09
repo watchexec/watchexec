29d94059299e830b02152da09e4241dd78172c56
Félix Saparelli <felix@passcod.name>
2025-02-09 21:35:54 +1300

    update manpage

--- crates/cli/integration/socket.sh
+++ crates/cli/integration/socket.sh
@@ -5,9 +5,12 @@ set -euxo pipefail
 watchexec=${WATCHEXEC_BIN:-watchexec}
 test_socketfd=${TEST_SOCKETFD_BIN:-test-socketfd}
 
-timeout -s9 10s sh -c "$watchexec --socket 18080 -1 -- $test_socketfd tcp"
-timeout -s9 10s sh -c "$watchexec --socket udp::18080 -1 -- $test_socketfd udp"
-timeout -s9 10s sh -c "$watchexec --socket 18080 --socket 28080 -1 -- $test_socketfd tcp tcp"
-timeout -s9 10s sh -c "$watchexec --socket 18080 --socket 28080 --socket udp::38080 -1 -- $test_socketfd tcp tcp udp"
-timeout -s9 10s sh -c "$watchexec --socket 127.0.1.1:18080 -1 -- $test_socketfd tcp"
+$watchexec --socket 18080 -1 -- $test_socketfd tcp
+$watchexec --socket udp::18080 -1 -- $test_socketfd udp
+$watchexec --socket 18080 --socket 28080 -1 -- $test_socketfd tcp tcp
+$watchexec --socket 18080 --socket 28080 --socket udp::38080 -1 -- $test_socketfd tcp tcp udp
+
+if [[ "$TEST_PLATFORM" = "linux" ]]; then
+	$watchexec --socket 127.0.1.1:18080 -1 -- $test_socketfd tcp
+fi
 
