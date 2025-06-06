848c0cd927b030b31dcf1a89733c474cc29a2785
Félix Saparelli <felix@passcod.name>
2025-06-07 04:15:11 +1200

    wip

--- crates/ignore-files/src/parse/git.rs
+++ crates/ignore-files/src/parse/git.rs
@@ -1,7 +1,4 @@
-use winnow::combinator::{opt, separated, seq};
-use winnow::prelude::*;
-use winnow::token::{literal, rest, take_until};
-use winnow::Result;
+use chumsky::prelude::*;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Pattern {
@@ -17,109 +14,73 @@ pub enum Segment {
 	All,
 }
 
-impl std::str::FromStr for Pattern {
-	type Err = String;
-
-	fn from_str(s: &str) -> Result<Self, Self::Err> {
-		pattern.parse(s).map_err(|e| e.to_string())
-	}
-}
-
-fn pattern(input: &mut &str) -> Result<Pattern> {
-	seq!(Pattern {
-		negated: opt(literal('!')).map(|l| l.is_some()),
-		segments: (
-			opt(literal('/')),
-			separated(0.., rest, '/'),
-			opt(literal('/'))
-		)
-			.map(
-				|(begin, segments, end): (Option<&str>, Vec<&str>, Option<&str>)| {
-					let mut segments: Vec<Segment> = segments
-						.into_iter()
-						.map(|segment| {
-							if segment == "**" {
-								Segment::All
-							} else if segment.contains(|c| c == '*' || c == '?' || c == '[') {
-								// "a string is a wildcard pattern if it contains one of the characters '?', '*', or '['"
-								Segment::Wildcard(segment.to_string())
-							} else {
-								Segment::Fixed(segment.to_string())
-							}
-						})
-						.collect();
-					if begin.is_some() {
-						segments.insert(0, Segment::Terminal);
-					}
-					if end.is_some() {
-						segments.push(Segment::Terminal);
-					}
-					segments
-				}
-			),
-	})
-	.parse_next(input)
+fn pattern<'src>() -> impl Parser<'src, &'src str, Pattern> {
+	none_of("/")
+		.repeated()
+		.collect::<String>()
+		.map(|seg| {
+			if seg.is_empty() {
+				Segment::Terminal
+			} else if seg == "**" {
+				Segment::All
+			} else if seg.contains(['*', '?', '[']) {
+				Segment::Wildcard(seg)
+			} else {
+				Segment::Fixed(seg)
+			}
+		})
+		.separated_by(just('/'))
+		.collect::<Vec<_>>()
+		.map(|segments| Pattern {
+			negated: false,
+			segments,
+		})
 }
 
 #[test]
 fn test_patterns() {
 	assert_eq!(
-		pattern.parse_peek("test"),
-		Ok((
-			"",
-			Pattern {
-				negated: false,
-				segments: vec![Segment::Fixed("test".into())],
-			}
-		))
+		pattern().parse("test").into_result(),
+		Ok(Pattern {
+			negated: false,
+			segments: vec![Segment::Fixed("test".into())],
+		})
 	);
 	assert_eq!(
-		pattern.parse_peek("/test"),
-		Ok((
-			"",
-			Pattern {
-				negated: false,
-				segments: vec![Segment::Terminal, Segment::Fixed("test".into())],
-			}
-		))
+		pattern().parse("/test").into_result(),
+		Ok(Pattern {
+			negated: false,
+			segments: vec![Segment::Terminal, Segment::Fixed("test".into())],
+		})
 	);
 	assert_eq!(
-		pattern.parse_peek("test/"),
-		Ok((
-			"",
-			Pattern {
-				negated: false,
-				segments: vec![Segment::Fixed("test".into()), Segment::Terminal],
-			}
-		))
+		pattern().parse("test/").into_result(),
+		Ok(Pattern {
+			negated: false,
+			segments: vec![Segment::Fixed("test".into()), Segment::Terminal],
+		})
 	);
 	assert_eq!(
-		pattern.parse_peek("/test/"),
-		Ok((
-			"",
-			Pattern {
-				negated: false,
-				segments: vec![
-					Segment::Terminal,
-					Segment::Fixed("test".into()),
-					Segment::Terminal
-				],
-			}
-		))
+		pattern().parse("/test/").into_result(),
+		Ok(Pattern {
+			negated: false,
+			segments: vec![
+				Segment::Terminal,
+				Segment::Fixed("test".into()),
+				Segment::Terminal
+			],
+		})
 	);
 	assert_eq!(
-		pattern.parse_peek("/foo/**/b*z"),
-		Ok((
-			"",
-			Pattern {
-				negated: false,
-				segments: vec![
-					Segment::Terminal,
-					Segment::Fixed("foo".into()),
-					Segment::All,
-					Segment::Wildcard("b*z".into())
-				],
-			}
-		))
+		pattern().parse("/foo/**/b*z").into_result(),
+		Ok(Pattern {
+			negated: false,
+			segments: vec![
+				Segment::Terminal,
+				Segment::Fixed("foo".into()),
+				Segment::All,
+				Segment::Wildcard("b*z".into())
+			],
+		})
 	);
 }
