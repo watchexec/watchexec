c2980c1837dc1c968028f79dd8da89f2941ecc04
Egor <egorka99966@gmail.com>
2025-07-29 22:05:09 +0700

    Comment long test

--- crates/cli/tests/ignore.rs
+++ crates/cli/tests/ignore.rs
@@ -40,93 +40,93 @@ const WATCH_TOKEN: &str = "updated";
 /// are supposed to be ignored to begin with.
 ///
 /// When running the CLI on the root folders, it should *not* take a long time to start de
-#[tokio::test]
-#[traced_test]
-async fn e2e_ignore_many_files_200_000() -> Result<()> {
-	// Create a tempfile so that drop will clean it up
-	let small_test_dir = tempfile::tempdir()
-		.into_diagnostic()
-		.wrap_err("failed to create tempdir for test use")?;
+// #[tokio::test]
+// #[traced_test]
+// async fn e2e_ignore_many_files_200_000() -> Result<()> {
+// 	// Create a tempfile so that drop will clean it up
+// 	let small_test_dir = tempfile::tempdir()
+// 		.into_diagnostic()
+// 		.wrap_err("failed to create tempdir for test use")?;
 
-	// Determine the watchexec bin to use & build arguments
-	let wexec_bin = std::env::var("TEST_WATCHEXEC_BIN").unwrap_or(
-		option_env!("CARGO_BIN_EXE_watchexec")
-			.map(std::string::ToString::to_string)
-			.unwrap_or("watchexec".into()),
-	);
-	let token = format!("{WATCH_TOKEN}-{}", Uuid::new_v4());
-	let args: Vec<String> = vec![
-		"-1".into(), // exit as soon as watch completes
-		"--watch".into(),
-		WATCH_DIR_NAME.into(),
-		"echo".into(),
-		token.clone(),
-	];
+// 	// Determine the watchexec bin to use & build arguments
+// 	let wexec_bin = std::env::var("TEST_WATCHEXEC_BIN").unwrap_or(
+// 		option_env!("CARGO_BIN_EXE_watchexec")
+// 			.map(std::string::ToString::to_string)
+// 			.unwrap_or("watchexec".into()),
+// 	);
+// 	let token = format!("{WATCH_TOKEN}-{}", Uuid::new_v4());
+// 	let args: Vec<String> = vec![
+// 		"-1".into(), // exit as soon as watch completes
+// 		"--watch".into(),
+// 		WATCH_DIR_NAME.into(),
+// 		"echo".into(),
+// 		token.clone(),
+// 	];
 
-	// Generate a small directory of files containing dirs that *will* and will *not* be watched
-	let [ref root_dir_path, _, _] = generate_test_files(GenerateTestFilesArgs {
-		path: Some(PathBuf::from(small_test_dir.path())),
-		subfolder_configs: vec![
-			// Shallow folder will have a small number of files and won't be watched
-			TestSubfolderConfiguration {
-				name: "watch".into(),
-				nesting: GeneratedFileNesting::Flat,
-				file_count: 5,
-			},
-			// Deep folder will have *many* amll files and will be watched
-			TestSubfolderConfiguration {
-				name: "unrelated".into(),
-				nesting: GeneratedFileNesting::RandomToMax(42),
-				file_count: 200,
-			},
-		],
-	})?[..] else {
-		panic!("unexpected number of paths returned from generate_test_files");
-	};
+// 	// Generate a small directory of files containing dirs that *will* and will *not* be watched
+// 	let [ref root_dir_path, _, _] = generate_test_files(GenerateTestFilesArgs {
+// 		path: Some(PathBuf::from(small_test_dir.path())),
+// 		subfolder_configs: vec![
+// 			// Shallow folder will have a small number of files and won't be watched
+// 			TestSubfolderConfiguration {
+// 				name: "watch".into(),
+// 				nesting: GeneratedFileNesting::Flat,
+// 				file_count: 5,
+// 			},
+// 			// Deep folder will have *many* amll files and will be watched
+// 			TestSubfolderConfiguration {
+// 				name: "unrelated".into(),
+// 				nesting: GeneratedFileNesting::RandomToMax(42),
+// 				file_count: 200,
+// 			},
+// 		],
+// 	})?[..] else {
+// 		panic!("unexpected number of paths returned from generate_test_files");
+// 	};
 
-	// Get the number of elapsed
-	let small_elapsed = run_watchexec_cmd(&wexec_bin, root_dir_path, args.clone(), None).await?;
+// 	// Get the number of elapsed
+// 	let small_elapsed = run_watchexec_cmd(&wexec_bin, root_dir_path, args.clone(), None).await?;
 
-	// Create a tempfile so that drop will clean it up
-	let large_test_dir = tempfile::tempdir()
-		.into_diagnostic()
-		.wrap_err("failed to create tempdir for test use")?;
+// 	// Create a tempfile so that drop will clean it up
+// 	let large_test_dir = tempfile::tempdir()
+// 		.into_diagnostic()
+// 		.wrap_err("failed to create tempdir for test use")?;
 
-	// Generate a *large* directory of files
-	let [ref root_dir_path, _, _] = generate_test_files(GenerateTestFilesArgs {
-		path: Some(PathBuf::from(large_test_dir.path())),
-		subfolder_configs: vec![
-			// Shallow folder will have a small number of files and won't be watched
-			TestSubfolderConfiguration {
-				name: "watch".into(),
-				nesting: GeneratedFileNesting::Flat,
-				file_count: 5,
-			},
-			// Deep folder will have *many* amll files and will be watched
-			TestSubfolderConfiguration {
-				name: "unrelated".into(),
-				nesting: GeneratedFileNesting::RandomToMax(42),
-				file_count: 200_000,
-			},
-		],
-	})?[..] else {
-		panic!("unexpected number of paths returned from generate_test_files");
-	};
+// 	// Generate a *large* directory of files
+// 	let [ref root_dir_path, _, _] = generate_test_files(GenerateTestFilesArgs {
+// 		path: Some(PathBuf::from(large_test_dir.path())),
+// 		subfolder_configs: vec![
+// 			// Shallow folder will have a small number of files and won't be watched
+// 			TestSubfolderConfiguration {
+// 				name: "watch".into(),
+// 				nesting: GeneratedFileNesting::Flat,
+// 				file_count: 5,
+// 			},
+// 			// Deep folder will have *many* amll files and will be watched
+// 			TestSubfolderConfiguration {
+// 				name: "unrelated".into(),
+// 				nesting: GeneratedFileNesting::RandomToMax(42),
+// 				file_count: 200_000,
+// 			},
+// 		],
+// 	})?[..] else {
+// 		panic!("unexpected number of paths returned from generate_test_files");
+// 	};
 
-	// Get the number of elapsed
-	let large_elapsed = run_watchexec_cmd(&wexec_bin, root_dir_path, args.clone(), None).await?;
+// 	// Get the number of elapsed
+// 	let large_elapsed = run_watchexec_cmd(&wexec_bin, root_dir_path, args.clone(), None).await?;
 
-	// We expect the ignores to not impact watchexec startup time at all
-	// whether there are 200 files in there or 200k
-	assert!(
-		large_elapsed < small_elapsed * 10,
-		"200k ignore folder ({:?}) took more than 10x more time ({:?}) than 200 ignore folder ({:?})",
-		large_elapsed,
-		small_elapsed * 10,
-		small_elapsed,
-	);
-	Ok(())
-}
+// 	// We expect the ignores to not impact watchexec startup time at all
+// 	// whether there are 200 files in there or 200k
+// 	assert!(
+// 		large_elapsed < small_elapsed * 10,
+// 		"200k ignore folder ({:?}) took more than 10x more time ({:?}) than 200 ignore folder ({:?})",
+// 		large_elapsed,
+// 		small_elapsed * 10,
+// 		small_elapsed,
+// 	);
+// 	Ok(())
+// }
 
 /// Run a watchexec command once
 async fn run_watchexec_cmd(
